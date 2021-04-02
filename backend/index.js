c549c727146d334f1ee574afef0a0a19155538f1
= <macd.collin@gmail.com>
2021-04-01 21:30:10 -0500

    Integrate FE and BE

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
-var express = require('express');var app = express();app.set('view engine', 'ejs');
+const cors = require('cors');
+var express = require('express');
 
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(cors());
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 app.listen(3001);
 
-app.get('/', function(req, res) {  res.render('home');});
+
+app.post('/submission', function(req, res) {
+    console.log(req.body.someData);
+    res.json({ test: 'test data' });
+});
